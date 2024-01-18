6721fa0d3907eb805f0ed25e3c396ad872c515a7
Matheus T. dos Santos <matheuswhite1@protonmail.com>
2024-01-18 12:08:41 -0300

    feat: removing ble and loopback interfaces

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
+#![deny(warnings)]
+
 extern crate core;
 
-use crate::ble::BleIF;
 use crate::command_bar::CommandBar;
-use crate::interface::Interface;
-use crate::loop_back::LoopBackIF;
 use crate::serial::SerialIF;
 use crate::theme::Theme;
-use btleplug::api::bleuuid::uuid_from_u16;
-use chrono::Local;
-use clap::{Parser, Subcommand};
+use clap::Parser;
 use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
 use crossterm::execute;
 use crossterm::terminal::{
@@ -17,16 +14,12 @@ use crossterm::terminal::{
 use std::io;
 use std::io::Stdout;
 use std::path::PathBuf;
-use std::time::Duration;
 use tui::backend::{Backend, CrosstermBackend};
 use tui::Terminal;
-use uuid::Uuid;
 
-mod ble;
 mod command_bar;
 mod error_pop_up;
 mod interface;
-mod loop_back;
 mod serial;
 mod text;
 mod theme;
@@ -36,58 +29,21 @@ type ConcreteBackend = CrosstermBackend<Stdout>;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    #[command(subcommand)]
-    interface: InterfacesArgs,
+    port: String,
+    baudrate: u32,
     #[clap(short, long)]
     view_length: Option<usize>,
     #[clap(short, long)]
     cmd_file: Option<PathBuf>,
 }
 
-#[derive(Subcommand)]
-enum InterfacesArgs {
-    Serial { port: String, baudrate: u32 },
-    Ble { name: String },
-    Loopback {},
-    Version {},
-}
-
 const CMD_FILEPATH: &str = "cmds.yaml";
 const CAPACITY: usize = 2000;
-const BLE_TX_UUID: Uuid = uuid_from_u16(0xAB02);
-const BLE_RX_UUID: Uuid = uuid_from_u16(0xAB01);
 
 fn main() -> Result<(), io::Error> {
     let cli = Cli::parse();
 
-    let loopback_graph_fn = || {
-        let now = 2.0 * std::f32::consts::PI * 1000.0;
-        let now = Local::now().timestamp_millis() % now as i64;
-        let now = now as f32 / 1000.0;
-        format!(
-            "{},{},\x07\x00\x01{},{},{}\x06,{} {}\n",
-            f32::sin(now),
-            f32::cos(now),
-            f32::sin(now) + f32::cos(now),
-            -f32::sin(now),
-            -f32::cos(now),
-            -f32::sin(now) + f32::cos(now),
-            "Hello".repeat(10),
-        )
-    };
-
-    let interface: Box<dyn Interface> = match &cli.interface {
-        InterfacesArgs::Serial { port, baudrate } => Box::new(SerialIF::new(port, *baudrate)),
-        InterfacesArgs::Ble { name } => Box::new(BleIF::new(BLE_TX_UUID, BLE_RX_UUID, &name)),
-        InterfacesArgs::Loopback {} => Box::new(LoopBackIF::new(
-            loopback_graph_fn,
-            Duration::from_millis(1000),
-        )),
-        InterfacesArgs::Version {} => {
-            println!("Version 0.1.0");
-            return Ok(());
-        }
-    };
+    let interface = Box::new(SerialIF::new(&cli.port, cli.baudrate));
 
     let view_length = cli.view_length.unwrap_or(CAPACITY);
     let cmd_file = cli.cmd_file.unwrap_or(PathBuf::from(CMD_FILEPATH));
