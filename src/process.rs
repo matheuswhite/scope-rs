72ea3d7f9f57ea9bb10f9518756431c597de6889
Matheus T. dos Santos <matheuswhite1@protonmail.com>
2024-03-14 09:32:56 -0300

    fix: fixes stdout and stderr read

--- src/process.rs
+++ src/process.rs
@@ -1,9 +1,10 @@
 use crate::plugin::{Plugin, PluginRequestResult};
 use crate::text::TextView;
-use std::io::{BufRead, BufReader};
-use std::process::{Command, Stdio};
+use std::process::Stdio;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::process::Command;
 use tokio::sync::Mutex;
 
 pub struct ProcessRunner {
@@ -50,31 +51,17 @@ impl ProcessRunner {
                 .map_err(|err| err.to_string())?
         };
 
-        static IS_END: AtomicBool = AtomicBool::new(false);
-        IS_END.store(false, Ordering::SeqCst);
         let text_view2 = self.text_view.clone();
         let text_view3 = self.text_view.clone();
         let plugin_name2 = plugin_name.clone();
         let stop_process_flag2 = stop_process_flag.clone();
-        let stop_process_flag3 = stop_process_flag.clone();
 
         let stdout = child.stdout.take().ok_or("Cannot get stdout".to_string())?;
         let mut stdout = BufReader::new(stdout).lines();
         let stdout_pipe = tokio::spawn(async move {
             let mut buffer = vec![];
 
-            while IS_END.load(Ordering::SeqCst) {
-                if stop_process_flag.load(Ordering::SeqCst) {
-                    return buffer;
-                }
-
-                if let Some(Ok(line)) = stdout.next() {
-                    buffer.push(line.clone());
-                    Plugin::println(text_view2.clone(), plugin_name.clone(), line.clone()).await;
-                }
-            }
-
-            while let Some(Ok(line)) = stdout.next() {
+            while let Ok(Some(line)) = stdout.next_line().await {
                 if stop_process_flag.load(Ordering::SeqCst) {
                     return buffer;
                 }
@@ -91,18 +78,7 @@ impl ProcessRunner {
         let stderr_pipe = tokio::spawn(async move {
             let mut buffer = vec![];
 
-            while IS_END.load(Ordering::SeqCst) {
-                if stop_process_flag2.load(Ordering::SeqCst) {
-                    return buffer;
-                }
-
-                if let Some(Ok(line)) = stderr.next() {
-                    buffer.push(line.clone());
-                    Plugin::eprintln(text_view3.clone(), plugin_name2.clone(), line).await;
-                }
-            }
-
-            while let Some(Ok(line)) = stderr.next() {
+            while let Ok(Some(line)) = stderr.next_line().await {
                 if stop_process_flag2.load(Ordering::SeqCst) {
                     return buffer;
                 }
@@ -113,17 +89,6 @@ impl ProcessRunner {
 
             buffer
         });
-
-        'wait_loop: while let Ok(None) = child.try_wait() {
-            if stop_process_flag3.load(Ordering::SeqCst) {
-                let _ = child.kill();
-                stdout_pipe.abort();
-                stderr_pipe.abort();
-                break 'wait_loop;
-            }
-        }
-
-        IS_END.store(true, Ordering::SeqCst);
 
         Ok(PluginRequestResult::Exec {
             stdout: stdout_pipe.await.unwrap(),
